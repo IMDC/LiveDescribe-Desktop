55fdd4d7ca70761ed346dea9f1b0dbb4af7ca40f
dmaugeri <dcc.maugeri@gmail.com>
2013-08-16 00:05:00 -0400

    fixed bug when control is resized
    - known bug marker seems to draw passed the audiocanvas height on startup

--- LiveDescribe/LiveDescribe/View/MainWindow.xaml.cs
+++ LiveDescribe/LiveDescribe/View/MainWindow.xaml.cs
@@ -13,8 +13,8 @@ namespace LiveDescribe.View
     {
         
         private double _videoDuration;
-        private double _audioCanvasHeight;
-        private double _audioCanvasWidth;
+        private double _audioTimeLineHeight;
+        private double _audioTimeLineWidth;
         private readonly DispatcherTimer _videoTimer;
 
         private int pageTime = 30; //30 seconds page time before audiocanvas  & descriptioncanvas scroll
@@ -27,7 +27,7 @@ namespace LiveDescribe.View
    
             _videoTimer = new DispatcherTimer();
             _videoTimer.Tick += Play_Tick;
-            //videoTimer.Interval = new TimeSpan(0, 0, 0, 0, 10);
+            _videoTimer.Interval = new TimeSpan(0, 0, 0, 1);
 
             DataContext = mc;
            
@@ -68,7 +68,7 @@ namespace LiveDescribe.View
             //update marker position from the video controller
             //something like this mc.VideoControl.CalculateMarkerPosition(TimeToMoveMarkerTo, pageSizeTime)
             
-            int position = (int) Math.Round((VideoMedia.Position.Seconds / _videoDuration) * _audioCanvasWidth);
+            int position = (int) Math.Round((VideoMedia.Position.Seconds / _videoDuration) * _audioTimeLineWidth);
             double initialPos = Canvas.GetLeft(Marker);
             double newPos = initialPos + 1;
             Canvas.SetLeft(Marker, newPos);
@@ -77,7 +77,7 @@ namespace LiveDescribe.View
             //draw marker at correct position
         }
 
-
+        #region View Listeners
         /// <summary>
         /// Updates the canvasWidth and canvasHeight variables everytime the canvas size is changed
         /// </summary>
@@ -85,31 +85,30 @@ namespace LiveDescribe.View
         /// <param name="e"></param>
         private void timeLine_SizeChanged_1(object sender, SizeChangedEventArgs e)
         {
-            _audioCanvasHeight = AudioCanvasBorder.ActualHeight;
-            _audioCanvasWidth = AudioCanvasBorder.ActualWidth;
-            //  Console.WriteLine("WIDTH: " + this.audioCanvasWidth);
-            //  Console.WriteLine("HEIGHT: " + this.audioCanvasHeight);
+            _audioTimeLineHeight = TimeLine.ActualHeight;
+            _audioTimeLineWidth = TimeLine.ActualWidth;
+            Console.WriteLine("WIDTH: " + _audioTimeLineWidth);
+            //Console.WriteLine("HEIGHT: " + this.audioCanvasHeight);
             SetPaging();
 
             //the 4th point is the bottom point of the marker, adjusting the y value of this point
-            Marker.Points[4] = new Point(Marker.Points[4].X, _audioCanvasHeight);
+            Marker.Points[4] = new Point(Marker.Points[4].X, AudioCanvasBorder.ActualHeight);
         }
+        #endregion
 
         #region Helper Functions
-
         /// <summary>
-        /// 
+        /// Sets the width of the audio canvas in the scrollview based off the pageTime and the total duration of the audio, if the AudioCanvas Width is greater
+        /// then the ActualWidth of the AudioCanvas then it scrolls to compensate for the additional width
         /// </summary>
         private void SetPaging()
         {
-            double pages = 30.093 / pageTime;
-            double width = AudioCanvasBorder.ActualWidth * pages;
+            double pages = (4.24 * 60) / pageTime;
+           // double width = AudioCanvasBorder.ActualWidth * pages;
+            double width = _audioTimeLineWidth*pages;
             //Console.WriteLine("Width: " + this.audioCanvasBorder.ActualWidth);
             AudioCanvas.Width = width;
-            
         }
-
-
         #endregion
        
     }
