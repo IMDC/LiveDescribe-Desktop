493e5e955bc29a3d8348f3cfc74fb4c441405adc
dmaugeri <dcc.maugeri@gmail.com>
2013-12-28 21:41:52 -0500

    Created a custom event args to pass the description to the proper event

---
+++ LiveDescribe/LiveDescribe/Events/AddDescriptionEventArgs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LiveDescribe.Model;
+namespace LiveDescribe.Events
+{
+    public class DescriptionEventArgs : EventArgs
+    {
+        private Description _description;
+
+        public DescriptionEventArgs(Description description)
+        {
+            this._description = description;
+        }
+
+        public Description Description
+        {
+            get
+            {
+                return _description;
+            }
+        }
+    }
+}
