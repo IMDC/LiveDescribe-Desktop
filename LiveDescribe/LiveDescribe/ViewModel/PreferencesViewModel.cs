b6324040793b3007ac88a158852836ec5bfbea34
Valentin Serrago <valentinserrago@gmail.com>
2014-07-29 12:34:08 -0400

    Move AudioSourceInfo to its own class

--- LiveDescribe/LiveDescribe/ViewModel/PreferencesViewModel.cs
+++ LiveDescribe/LiveDescribe/ViewModel/PreferencesViewModel.cs
@@ -10,7 +10,6 @@ using System;
 using System.Collections.ObjectModel;
 using System.Globalization;
 using System.Linq;
-using System.Runtime.Serialization;
 using System.Windows;
 using System.Windows.Input;
 
@@ -21,65 +20,6 @@ namespace LiveDescribe.ViewModel
         #region Logger
         private static readonly log4net.ILog Log = log4net.LogManager.GetLogger
             (System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
-        #endregion
-
-        #region Inner Classes
-
-        public class AudioSourceInfo : ISerializable
-        {
-            public WaveInCapabilities Source { set; get; }
-            public string Name { set; get; }
-            public string Channels { set; get; }
-            public int DeviceNumber { set; get; }
-            public AudioSourceInfo(string name, string channels, WaveInCapabilities source, int deviceNumber)
-            {
-                Name = name;
-                Channels = channels;
-                Source = source;
-                DeviceNumber = deviceNumber;
-            }
-
-            public void GetObjectData(SerializationInfo info, StreamingContext context)
-            {
-                info.AddValue("source", Source, typeof(WaveInCapabilities));
-            }
-
-            public AudioSourceInfo(SerializationInfo info, StreamingContext context)
-            {
-                Source =
-                    (WaveInCapabilities)info.GetValue("source", typeof(WaveInCapabilities));
-            }
-
-            public override bool Equals(object obj)
-            {
-                if (obj == null)
-                    return false;
-
-                var info = obj as AudioSourceInfo;
-                if (info == null)
-                    return false;
-
-                return (Name == info.Name)
-                    && (Channels == info.Channels)
-                    && (DeviceNumber == info.DeviceNumber);
-            }
-
-            public bool Equals(AudioSourceInfo info)
-            {
-                if (info == null)
-                    return false;
-
-                return (Name == info.Name)
-                    && (Channels == info.Channels)
-                    && (DeviceNumber == info.DeviceNumber);
-            }
-
-            public override int GetHashCode()
-            {
-                String str = Name + Channels + DeviceNumber;
-                return str.GetHashCode();
-            }
-        }
         #endregion
 
         #region Instance Variables
