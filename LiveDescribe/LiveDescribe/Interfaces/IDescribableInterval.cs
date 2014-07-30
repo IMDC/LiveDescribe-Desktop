903c526d0e090f3eed7ad15ee70b2979f0fa885d
Valentin Serrago <valentinserrago@gmail.com>
2014-07-30 12:24:04 -0400

    Add other properties to shared interface

--- LiveDescribe/LiveDescribe/Interfaces/IDescribableInterval.cs
+++ LiveDescribe/LiveDescribe/Interfaces/IDescribableInterval.cs
@@ -1,10 +1,21 @@
-﻿namespace LiveDescribe.Interfaces
+﻿using System.Windows.Media;
+
+namespace LiveDescribe.Interfaces
 {
     /// <summary>
     /// Represents a period of time that can be described in some way
     /// </summary>
     public interface IDescribableInterval
     {
+        bool IsSelected { set; get; }
+        double X { set; get; }
+        double Y { set; get; }
+        double Height { set; get; }
+        double Width { set; get; }
+        double StartInVideo { set; get; }
+        double EndInVideo { set; get; }
+        double Duration { get; }
+        Color Colour { set; get; }
         string Text { set; get; }
     }
 }
