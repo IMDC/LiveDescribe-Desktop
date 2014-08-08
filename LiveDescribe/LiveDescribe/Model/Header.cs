e7383dc078882497e269f7e0e06bffe12c777309
Valentin Serrago <valentinserrago@gmail.com>
2014-08-08 14:04:41 -0400

    Fix calculation of audio duration
    Program should now find spaces throughout the entire movie.

--- LiveDescribe/LiveDescribe/Model/Header.cs
+++ LiveDescribe/LiveDescribe/Model/Header.cs
@@ -21,12 +21,41 @@ namespace LiveDescribe.Model
         public static readonly char[] FormatChunkMarker = { 'f', 'm', 't', ' ' };
         public static readonly char[] DataChunkMarker = { 'd', 'a', 't', 'a' };
 
+        /// <summary>
+        /// The size of the entire wave file with the header included, in bytes.
+        /// </summary>
         public uint FileSize;
+
+        /// <summary>
+        /// Number of Audio Channels in the wave file.
+        /// </summary>
         public ushort Channels;
+
+        /// <summary>
+        /// Audio sample rate of the wave file.
+        /// </summary>
         public uint SampleRate;
+
+        /// <summary>
+        /// The number of bytes per second of data this file contains. Formula is
+        /// (Sample Rate * BitsPerSample * Channels) / 8.
+        /// </summary>
         public uint ByteRate;
+
+        /// <summary>
+        /// The number of bytes per sample for all channels. Formula is
+        /// NumChannels * BitsPerSample/8.
+        /// </summary>
         public ushort BlockAlign;
+
+        /// <summary>
+        /// The number of bits for a single channel sample. Usually 16.
+        /// </summary>
         public ushort BitsPerSample;
+
+        /// <summary>
+        /// The size of the sound sample data. This does not include the header file.
+        /// </summary>
         public uint DataSize;
     }
 }
